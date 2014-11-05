9422ae5dc2b393c3b016679f2552dfcfcef48a35
Mico Micic <mico@micic.ch>
2014-11-05 19:21:57 +0100

    The login page

---
+++ src/bookstore-web/src/main/java/org/books/presentation/LoginBean.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.books.presentation;
+
+import java.io.Serializable;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+
+
+/**
+ *
+ * @author micic
+ */
+@Named("loginBean")
+@SessionScoped
+public class LoginBean implements Serializable {
+    
+    private String username;
+    
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    } 
+    
+    public void doLogin() {
+        
+    }
+}
