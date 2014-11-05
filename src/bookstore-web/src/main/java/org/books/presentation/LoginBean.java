aa811d3096379b6712160ab186f2b2c5bba98fc0
Mico Micic <mico@micic.ch>
2014-11-05 20:02:11 +0100

    Login messages

--- src/bookstore-web/src/main/java/org/books/presentation/LoginBean.java
+++ src/bookstore-web/src/main/java/org/books/presentation/LoginBean.java
@@ -6,9 +6,16 @@
 package org.books.presentation;
 
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
-
+import javax.inject.Inject;
+import org.books.application.Bookstore;
+import org.books.application.MessageFactory;
+import org.books.application.exception.InvalidCredentialsException;
+import org.books.type.EnumActionResult;
+import org.books.type.MessageKey;
 
 /**
  *
@@ -17,28 +24,40 @@ import javax.enterprise.context.SessionScoped;
 @Named("loginBean")
 @SessionScoped
 public class LoginBean implements Serializable {
-    
-    private String username;
-    
+
+    @Inject
+    private Bookstore bookstore;
+
+    private String email;
+
     private String password;
 
     public String getPassword() {
         return password;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String username) {
+        this.email = username;
     }
 
     public void setPassword(String password) {
         this.password = password;
-    } 
-    
-    public void doLogin() {
+    }
+
+    public String doLogin() {
         
+        try {
+            bookstore.authenticateCustomer(this.email, this.password);
+        } catch (InvalidCredentialsException ex) {
+            MessageFactory.error(MessageKey.INVALID_USER);
+            return null;
+        }
+        
+        MessageFactory.info(MessageKey.LOGIN_SUCCESS);
+        return null;
     }
 }
--- src/bookstore-web/src/main/java/org/books/type/MessageKey.java
+++ src/bookstore-web/src/main/java/org/books/type/MessageKey.java
@@ -12,7 +12,9 @@ package org.books.type;
 public enum MessageKey {
 
     BOOK_NOT_FOUND_BY_ISDN("org.books.presentation.CatalogBean.BOOK_NOT_FOUND"),
-    ORDER_CONFIRMED("org.books.presentation.CartBean.ORDER_CONFIRMED");
+    ORDER_CONFIRMED("org.books.presentation.CartBean.ORDER_CONFIRMED"),
+    INVALID_USER("org.books.presentation.LoginBean.INVALID_USER"),
+    LOGIN_SUCCESS("org.books.presentation.LoginBean.LOGIN_SUCCESS");
 
     private final String key;
 
