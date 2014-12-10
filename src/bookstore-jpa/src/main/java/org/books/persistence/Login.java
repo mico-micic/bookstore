ca1a4b677fdd8ef2408e10b704dad9d37e18b6f8
Thomas <sigibe@msn.com>
2014-12-10 19:57:06 +0100

    Login Nachcommit

---
+++ src/bookstore-jpa/src/main/java/org/books/persistence/Login.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.books.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Sigi
+ */
+@Entity
+public class Login extends IdentifiableObject {
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Login() {
+    }
+
+    public Login(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
