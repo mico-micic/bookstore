5ad4488dd4713106a96c0bc9520834b8186ea044
Mico Micic <mico@micic.ch>
2014-12-12 22:04:00 +0100

    New tests for CustomerDao

--- src/bookstore-jpa/src/test/java/org/books/persistence/TestDataFactory.java
+++ src/bookstore-jpa/src/test/java/org/books/persistence/TestDataFactory.java
@@ -30,10 +30,21 @@ public class TestDataFactory {
         createNewBook("Java 8 - Die Neuerungen", "978-3527710706", new BigDecimal(105.50), "Jutta Schmidt, Barry Burd", "Galileo Computing");
         createNewBook("Java für Dummies", "978-3836217880", new BigDecimal(105.50), "Hans-Peter Habelitz", "Carl Hanser Verlag GmbH");
 
-        Login login = createLogin("superuser@email.com", "pass@word");
-        CreditCard masterCard = createMasterCard("5105105105105100");
-        Address address = createAddress();
-        Customer customer = createCustomer(login, masterCard, address);
+        Login login1 = createLogin("superuser@email.com", "pass@word");
+        CreditCard masterCard1 = createMasterCard("5105105105105100");
+        Address address1 = createAddress();
+
+        Login login2 = createLogin("hans@wurst.ch", "secret_word");
+        CreditCard masterCard2 = createMasterCard("5105105105105100");
+        Address address2 = createAddress();
+        
+        Login login3 = createLogin("bonds_mother@007.ch", "secret_word");
+        CreditCard masterCard3 = createMasterCard("5105105105105100");
+        Address address3 = createAddress();
+
+        createCustomer("James", "Bond", login1, masterCard1, address1);
+        createCustomer("Hans", "Wurst", login2, masterCard2, address2);
+        createCustomer("Bonds_Mother", "Some_Name", login3, masterCard3, address3);
 
         // TODO complete me
         em.getTransaction().begin();
@@ -80,14 +91,14 @@ public class TestDataFactory {
         return address;
     }
 
-    private Customer createCustomer(Login login, CreditCard masterCard, Address address) {
+    private Customer createCustomer(String firstName, String lastName, Login login, CreditCard masterCard, Address address) {
         Customer customer = new Customer();
 
         // Wie abgemacht entspricht der UserName immer der EMail-Adresse:
         customer.setEmail(login.getUserName());
 
-        customer.setFirstName("James");
-        customer.setLastName("Bond");
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
         customer.setAddress(address);
         customer.setCreditCard(masterCard);
         em.persist(customer);
