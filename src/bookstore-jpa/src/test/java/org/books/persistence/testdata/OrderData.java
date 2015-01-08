4d95dd7d9697b8ac3aa115e31ec450a1467b55b8
Mico Micic <mico@micic.ch>
2015-01-08 22:37:10 +0100

    Added missing test-data value.

--- src/bookstore-jpa/src/test/java/org/books/persistence/testdata/OrderData.java
+++ src/bookstore-jpa/src/test/java/org/books/persistence/testdata/OrderData.java
@@ -23,6 +23,7 @@ public enum OrderData {
     O_1111_003("1111-003", BigDecimal.valueOf(16.99), Date.from(LocalDateTime.of(2010, Month.JANUARY, 25, 0, 0).toInstant(ZoneOffset.UTC))),
     O_1111_004("1111-004", BigDecimal.valueOf(77.20), Date.from(LocalDateTime.of(2011, Month.JUNE, 15, 0, 0).toInstant(ZoneOffset.UTC))), 
     O_2222_001("2222-001", BigDecimal.valueOf(28.15), Date.from(LocalDateTime.of(2014, Month.SEPTEMBER, 23, 0, 0).toInstant(ZoneOffset.UTC))),
+    ORDER_WITHOUT_LINE_ITEMS("2222-001", BigDecimal.valueOf(28.15), Date.from(LocalDateTime.of(2014, Month.SEPTEMBER, 23, 0, 0).toInstant(ZoneOffset.UTC))),
     O_3333_001("3333-001", BigDecimal.valueOf(77.10), Date.from(LocalDateTime.of(2014, Month.SEPTEMBER, 23, 0, 0).toInstant(ZoneOffset.UTC)));        
     
     private final String number;
