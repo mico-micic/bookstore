f0f01db5b0a2c042e423f3a5431c49ce0524bb20
Thomas <sigibe@msn.com>
2015-01-11 12:03:09 +0100

    Testharness fixed und erweitert

--- src/bookstore-ejb-test/src/test/java/org/books/ejb/impl/OrderServiceBeanTest.java
+++ src/bookstore-ejb-test/src/test/java/org/books/ejb/impl/OrderServiceBeanTest.java
@@ -1,7 +1,6 @@
 package org.books.ejb.impl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import javax.ejb.EJBException;
 import javax.naming.InitialContext;
@@ -21,7 +20,7 @@ import org.books.persistence.testdata.AbstractTestBase;
 import org.books.persistence.testdata.CustomerData;
 import org.books.persistence.testdata.OrderData;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 public class OrderServiceBeanTest extends AbstractTestBase {
@@ -29,11 +28,11 @@ public class OrderServiceBeanTest extends AbstractTestBase {
     private static final String ORDER_SERVICE_NAME = "java:global/bookstore-ear/bookstore-ejb/OrderService";
     private static final String CUSTOMER_SERVICE_NAME = "java:global/bookstore-ear/bookstore-ejb/CustomerService";
 
-    private static OrderService orderService;
-    private static CustomerService customerService;
+    private OrderService orderService;
+    private CustomerService customerService;
 
-    @BeforeClass
-    public static void setup() throws Exception {
+    @Before
+    public void before() throws Exception {
         orderService = (OrderService) new InitialContext().lookup(ORDER_SERVICE_NAME);
         customerService = (CustomerService) new InitialContext().lookup(CUSTOMER_SERVICE_NAME);
     }
@@ -45,12 +44,30 @@ public class OrderServiceBeanTest extends AbstractTestBase {
         Assert.assertEquals(Long.valueOf(10200L), order.getId());
     }
 
+    @Test(expected = OrderNotFoundException.class)
+    public void testFindOrderWithInvalidOrderId() throws Throwable {
+        try {
+            orderService.findOrder(111L);
+        } catch (EJBException ex) {
+            throw ex.getCause();
+        }
+    }
+
     @Test
     public void testFindOrderWithOrderNumber() throws OrderNotFoundException {
         String number = OrderData.ORDER_WITH_LINE_ITEMS.number();
         Order order = orderService.findOrder(number);
         Assert.assertNotNull(order);
         Assert.assertEquals(number, order.getNumber());
+    }
+
+    @Test(expected = OrderNotFoundException.class)
+    public void testFindOrderWithInvalidOrderNumber() throws Throwable {
+        try {
+            orderService.findOrder("");
+        } catch (EJBException ex) {
+            throw ex.getCause();
+        }
     }
 
     @Test
@@ -62,6 +79,42 @@ public class OrderServiceBeanTest extends AbstractTestBase {
         Assert.assertEquals(Order.Status.accepted, orderInfo.getStatus());
 
         Assert.assertNotNull(orderService.findOrder(orderInfo.getId()));
+    }
+
+    @Test(expected = CustomerNotFoundException.class)
+    public void testPlaceOrderWithInvalidCustomer() throws Throwable {
+        try {
+            orderService.placeOrder(1L, someValidOrderItems());
+        } catch (EJBException ex) {
+            throw ex.getCause();
+        }
+    }
+
+    @Test(expected = BookNotFoundException.class)
+    public void testPlaceOrderWithInvalidBook() throws Throwable {
+        Customer customer = customerService.findCustomer(CustomerData.HANS_WURST.email());
+        List<OrderItem> orderItems = someValidOrderItems();
+        orderItems.add(new OrderItem("AN_INVALID_ISBN", 7));
+        try {
+            orderService.placeOrder(customer.getId(), orderItems);
+        } catch (EJBException ex) {
+            throw ex.getCause();
+        }
+    }
+
+    @Test(expected = PaymentFailedException.class)
+    public void testPlaceOrderWithInvalidPaymentInfos() throws Throwable {
+        Customer customer = customerService.findCustomer(CustomerData.HANS_WURST.email());
+        customer.setId(null);
+        customer.setEmail("someArbitraryEMail@jaf0.ch");
+        customer.getCreditCard().setNumber("AN_INVALID_CREDIT_CARD_NUMBER");
+        Long registeredCustomerId = customerService.registerCustomer(customer, "bliBlaBlu-Password");
+
+        try {
+            orderService.placeOrder(registeredCustomerId, someValidOrderItems());
+        } catch (EJBException ex) {
+            throw ex.getCause();
+        }
     }
 
     @Test(expected = InvalidOrderStatusException.class)
@@ -76,7 +129,9 @@ public class OrderServiceBeanTest extends AbstractTestBase {
         Assert.assertNotNull(order);
         orderService.cancelOrder(order.getId());
 
-        Assert.assertEquals(Order.Status.canceled, order.getStatus());
+        order = orderService.findOrder(order.getId());
+        Assert.assertEquals("Order-Status must be canceled after canceling the order.",
+                Order.Status.canceled, order.getStatus());
 
         try {
             orderService.cancelOrder(order.getId());
