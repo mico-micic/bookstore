f8e9e49114a7b76dea81be3311736ce538e317da
Thomas <sigibe@msn.com>
2015-01-21 19:08:26 +0100

    bla

--- src/bookstore-ejb-test/src/test/java/org/books/ejb/impl/ArquillianTest.java
+++ src/bookstore-ejb-test/src/test/java/org/books/ejb/impl/ArquillianTest.java
@@ -5,6 +5,7 @@
  */
 package org.books.ejb.impl;
 
+import java.io.File;
 import javax.ejb.EJB;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -26,9 +27,15 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenResolvedArtifact;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenResolverSystem;
+import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -41,34 +48,44 @@ public class ArquillianTest {
 
     @Deployment
     public static Archive<?> createDeployment() {
-        JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
-                .addClass(CatalogService.class)
-                .addClass(CatalogServiceLocal.class)
-                .addPackage(CatalogServiceRemote.class.getPackage())
-                .addPackage(CatalogServiceBean.class.getPackage())
-                .addPackage(BookNotFoundException.class.getPackage())
-                .addPackage(BookDao.class.getPackage())
-                .addPackage(Customer.class.getPackage())
-                .addPackage(Logger.class.getPackage())
-                .addPackage(AppenderAttachable.class.getPackage())
-                .addPackage(OrderInfo.class.getPackage())
-                .addPackage(AesEncryptorConverter.class.getPackage())
-                .addAsManifestResource("META-INF/persistence-arquillian.xml", "persistence.xml")
-                .addAsManifestResource("META-INF/queries.xml", "queries.xml")
-                .addAsManifestResource("META-INF/converters.xml", "converters.xml")
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-        
-        System.out.println(jar.toString(true));
 
-        return jar;
+        PomEquippedResolveStage resolver = Maven
+                .resolver()
+                .loadPomFromFile("pom.xml");
+
+        EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class);
+
+        ear.addAsLibraries(resolver
+                .resolve("org.books.persistence:bookstore-jpa")
+                .withTransitivity()
+                .asFile());
+
+        ear.addAsLibraries(resolver
+                .resolve("org.bookstore:bookstore-ejb")
+                .withTransitivity()
+                .asFile());
+
+        ear.addAsLibraries(resolver
+                .resolve("log4j:log4j:1.2.17")
+                .withoutTransitivity()
+                .asFile());
+
+        ear.addAsLibraries(
+                ShrinkWrap
+                .create(JavaArchive.class)
+                .addPackage(ArquillianTest.class.getPackage()));
+
+        System.out.println(ear.toString(true));
+
+        return ear;
     }
 
     @EJB
     CustomerServiceLocal customerService;
-    
+
     @Test
-    public void AlbumRepository_should_return_album_by_id(){
-     
+    public void AlbumRepository_should_return_album_by_id() {
+
         Assert.assertNotNull(customerService);
     }
 
