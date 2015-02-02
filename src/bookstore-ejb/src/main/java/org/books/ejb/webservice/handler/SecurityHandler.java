2a32c0236e2d87e60ba68c2bd7a4912c5b96b552
Mico Micic <mico@micic.ch>
2015-02-02 16:27:31 +0100

    Added amazon webservice skeleton.

---
+++ src/bookstore-ejb/src/main/java/org/books/ejb/webservice/handler/SecurityHandler.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.books.ejb.webservice.handler;
+
+import java.util.Set;
+import javax.xml.namespace.QName;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+/**
+ *
+ * @author micic
+ */
+public class SecurityHandler implements SOAPHandler<SOAPMessageContext> {
+
+    @Override
+    public Set<QName> getHeaders() {
+        return null;
+    }
+
+    @Override
+    public boolean handleMessage(SOAPMessageContext context) {
+        return true;
+    }
+
+    @Override
+    public boolean handleFault(SOAPMessageContext context) {
+        return true;
+    }
+
+    @Override
+    public void close(MessageContext context) {
+
+    }
+}
