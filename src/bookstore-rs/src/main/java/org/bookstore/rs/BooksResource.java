e41b310219001f73a5997d60f82de8368be9b36c
Thomas <sigibe@msn.com>
2015-02-11 20:00:58 +0100

    BooksResource

--- src/bookstore-rs/src/main/java/org/bookstore/rs/BooksResource.java
+++ src/bookstore-rs/src/main/java/org/bookstore/rs/BooksResource.java
@@ -7,19 +7,17 @@ package org.bookstore.rs;
 
 import java.util.List;
 import javax.ejb.EJB;
-import javax.ejb.EJBException;
-import javax.faces.bean.ManagedBean;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
 import org.books.ejb.CatalogServiceLocal;
 import org.books.ejb.exception.BookNotFoundException;
 import org.books.persistence.entity.Book;
-import org.bookstore.rs.exception.BookNotFoundWebAppException;
 
 /**
  * @author Sigi
@@ -34,12 +32,24 @@ public class BooksResource {
     @Path("{id}")
     @Consumes({"application/xml", "application/json"})
     @Produces({"application/xml", "application/json"})
-    public Book find(@PathParam("id") Long id) {
-        System.out.println("Triggered BookResource.find(ID)");
+    public Book findById(@PathParam("id") Long id) {
+        validateNotNull(id);
         try {
             return catalogService.findBook(id);
         } catch (BookNotFoundException ex) {
-            throw new BookNotFoundWebAppException();
+            throw new NotFoundException();
+        }
+    }
+
+    @GET
+    @Consumes({"application/xml", "application/json"})
+    @Produces({"application/xml", "application/json"})
+    public Book findByIsbn(@QueryParam("isbn") String isbn) {
+        validateNotBlank(isbn);
+        try {
+            return catalogService.findBook(isbn);
+        } catch (BookNotFoundException ex) {
+            throw new NotFoundException();
         }
     }
 
@@ -48,23 +58,18 @@ public class BooksResource {
     @Consumes({"application/xml", "application/json"})
     @Produces({"application/xml", "application/json"})
     public List<Book> search(@QueryParam("keywords") String keywords) {
-        System.out.println("Triggered BookResource.search(keywords)");
         return catalogService.searchBooks(keywords);
     }
 
-    private WebApplicationException extractedException(Throwable t) {
-        if (t instanceof EJBException) {
-            return extractEJBExceptions((EJBException) t);
-        } else {
-            throw new WebApplicationException(t);
+    private void validateNotNull(Object id) throws BadRequestException {
+        if (id == null) {
+            throw new BadRequestException();
         }
     }
 
-    private WebApplicationException extractEJBExceptions(EJBException ex) {
-        if (ex.getCause() instanceof BookNotFoundException) {
-            return new BookNotFoundWebAppException();
-        } else {
-            return new WebApplicationException(ex);
+    private void validateNotBlank(String str) {
+        if (str == null || "".equals(str.trim())) {
+            throw new BadRequestException();
         }
     }
 
